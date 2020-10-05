2d8929d4ae0981bed139196eb33644e6184fce28
Michael Hudson <mikehudson19@live.co.uk>
2020-10-05 15:36:29 +0200

    Update CalculateTotal methods and the Date generation functionality.

--- CSharp.App/Program.cs
+++ CSharp.App/Program.cs
@@ -8,9 +8,21 @@ namespace CSharp.App
     {
         static void Main(string[] args)
         {
-            List<Invoice> InvoiceList = new List<Invoice>() { new Invoice("A001", 50, 50), new Invoice("A002", 10, 500), new Invoice("A003", 8, 750) };
-            Console.WriteLine(InvoiceList[1].InvoiceNumber);
+            List<Invoice> InvoiceList = new List<Invoice>() { new Invoice("A001", 2, 0 ),
+                                                              new Invoice("A002", 1, 1),
+                                                              new Invoice("A003", 3, 2),
+                                                              new Invoice("A004", 3, 3),
+                                                              new Invoice("A003", 3, 4),
+                                                              new Invoice("A003", 3, 5),
+                                                              new Invoice("A003", 3, 6),
+                                                              new Invoice("A003", 3, 7),
+                                                              new Invoice("A003", 3, 8),
+                                                              new Invoice("A003", 3, 9),
+                                                              new Invoice("A003", 3, 10),
+                                                              new Invoice("A003", 3, 11),
+            };
 
+            
             Console.WriteLine("App is running....press any key to exit");
             Console.ReadLine();
         }
--- CSharp.Domain/Invoice.cs
+++ CSharp.Domain/Invoice.cs
@@ -12,35 +12,85 @@ namespace CSharp.Domain
         public double Num1 { get; set; }
         public double Num2 { get; set; }
         public List<InvoiceItem> InvoiceItems = new List<InvoiceItem>();
+        public int NoOfItems;
+        public int DateVar;
 
         public Invoice()
         {
-            this.InvoiceDate = DateTimeOffset.Now;
-            this.DueDate = DateTimeOffset.Now;
-            GenerateItems();
+           
         }
 
-        public Invoice(string invoiceNumber, double num1, double num2) : this()
+        public Invoice(string invoiceNumber, int noOfItems, int dateVar) : this()
         {
-            this.InvoiceNumber = invoiceNumber;
-            this.Num1 = num1;
-            this.Num2 = num2;
-            this.InvoiceTotal = CalculateTotal();
+            InvoiceNumber = invoiceNumber;
+            GenerateItems(noOfItems);
+            InvoiceTotal = CalculateTotal();
+            InvoiceDate = GetDate(dateVar);
+            DueDate = GetDueDate(dateVar); 
         }
 
         public double CalculateTotal()
         {
-            return Num1 * Num2;
-            // Loop through the Dictionary of items
-            // Access the item total               
-            // Add each total to the InvoiceTotal  
+            double total = 0;
+            foreach (var i in InvoiceItems)
+            {
+               total += i.Total;
+            }
+            return total;
         }
 
-        public void GenerateItems()
+        public void GenerateItems(int noOfItems)
         {
-            InvoiceItems.Add(new InvoiceItem("Development", 5, 800, "Software development" ));
-            InvoiceItems.Add(new InvoiceItem("Design", 5, 600, "Software design" ));
-            InvoiceItems.Add(new InvoiceItem("Digital Marketing", 5, 750, "Pushing products"));
+            if (noOfItems == 1)
+            {
+                InvoiceItems.Add(new InvoiceItem("Development", 5, 800, "Software development"));
+            }
+
+            if (noOfItems == 2)
+            {
+                InvoiceItems.Add(new InvoiceItem("Development", 5, 800, "Software development"));
+                InvoiceItems.Add(new InvoiceItem("Design", 5, 600, "Software design"));
+            }
+
+            if (noOfItems == 3)
+            {
+                InvoiceItems.Add(new InvoiceItem("Development", 5, 800, "Software development"));
+                InvoiceItems.Add(new InvoiceItem("Design", 5, 600, "Software design"));
+                InvoiceItems.Add(new InvoiceItem("Digital Marketing", 5, 750, "Pushing products"));
+            }
+        }
+
+        public DateTime GetDate(int dateVar)
+        {
+            var month = DateTime.UtcNow.AddMonths(dateVar).Month;
+
+            int year;
+
+            if ((12 - DateTime.UtcNow.Month) >= dateVar)
+            {
+               year = DateTime.UtcNow.Year;
+
+            }
+            else
+            {
+               year = DateTime.UtcNow.AddYears(1).Year;
+            }
+
+            var day = DateTime.DaysInMonth(year, month) - 5;
+
+            var myDate = new DateTime(year, month, day, 0, 0, 0);
+            return myDate;
+
+        
+        }
+
+        public DateTime GetDueDate(int invoiceDate)
+        {
+            var invoicedDate = GetDate(invoiceDate +1);
+            var nextMonth = invoicedDate.Month;
+            var year = invoicedDate.Year;
+            var day = DateTime.DaysInMonth(year, nextMonth);
+            return new DateTime(year, nextMonth, day, 0, 0, 0);
 
         }
     }
