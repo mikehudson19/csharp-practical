e93529cb478c57014a74549dab6839847c9981f8
Michael Hudson <mikehudson19@live.co.uk>
2020-10-05 11:44:04 +0200

    Initial commit - add invoice class.

---
+++ CSharp.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using CSharp.Domain;
+
+namespace CSharp.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Invoice> InvoiceList = new List<Invoice>() { new Invoice("A001"), new Invoice("A002"), new Invoice("A003") };
+            Console.WriteLine(InvoiceList[1].InvoiceNumber);
+
+            Console.WriteLine("App is running....press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp.Domain/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp.Domain
+{
+    public class Invoice
+    {
+        public DateTimeOffset InvoiceDate { get; set; }
+        public string InvoiceNumber { get; set; }
+        public DateTimeOffset DueDate { get; set; }
+        public double InvoiceTotal { get; set; }
+
+        public Invoice()
+        {
+            this.InvoiceDate = DateTimeOffset.Now;
+            this.DueDate = DateTimeOffset.Now;
+            this.InvoiceTotal = 500;
+        }
+
+        public Invoice(string invoiceNumber) : this()
+        {
+            this.InvoiceNumber = invoiceNumber;
+
+        }
+    }
+}
